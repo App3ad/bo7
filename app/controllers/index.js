bf806f1bb1fa78ebb18fa1aaf361bedc994b2ac4
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-09 21:46:15 +0200

    Code formate

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,15 +4,26 @@ Alloy.Globals.pageStack.open($.index);
 
 function transfomer(model) {
   var transform = model.toJSON();
+  // Set the color of each bank
   transform.bankColor = Alloy.CFG.banks[transform.bankCode].color;
+
+  // Set the color of the broduct
   transform.typeColor = Alloy.CFG.productType[transform.typeCode].color;
+
+  // Calculate the time from now
   transform.time = moment(transform.lastPaymentDate).add(transform.periodNumber, transform.periodType).fromNow();
-  transform.period = parseInt(transform.periodNumber) + transform.periodType.substr(0,1).toUpperCase();
+
+  // Get the period string, number + only 1st char in upper case
+  transform.period = parseInt(transform.periodNumber) + transform.periodType.substr(0, 1).toUpperCase();
+
   if (transform.period === '1Y') {
-    transform.period = 'A'; // Anual
+    // If 1Y, replace with A for Annual payment
+    transform.period = 'A';
   } else if (transform.period === '3M') {
+    // If 3M, replace with Q for Quartarly payment
     transform.period = 'Q'; // Quartarly
   } else if (transform.period === '6M') {
+    // If 6M, replace with 2Q for bi-Quartarly payment
     transform.period = '2Q'; // Quartarly
   }
   return transform;
