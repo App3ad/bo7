084eea36ad8b8f5d7a0408952445aaaf6b928cd4
Hazem Khaled <hazem.khaled@gmail.com>
2015-10-10 13:10:31 +0200

    Move moment load to global, we need it in another place

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,3 @@
-var moment = require('alloy/moment');
-
 Alloy.Globals.pageStack.open($.index);
 
 function transfomer(model) {
@@ -19,7 +17,7 @@ function transfomer(model) {
   }
 
   // Calculate the time from now
-  transform.time = moment(transform.lastPaymentDate).add(transform.periodNumber, transform.periodType).fromNow();
+  transform.time = Alloy.Globals.moment(transform.lastPaymentDate).add(transform.periodNumber, transform.periodType).fromNow();
 
   // Get the period string, number + only 1st char in upper case
   transform.period = parseInt(transform.periodNumber) + transform.periodType.substr(0, 1).toUpperCase();
@@ -38,6 +36,11 @@ function transfomer(model) {
 }
 
 function productListClicked(e) {
+  // dd button
+  if (e.bindId === 'addBtn') {
+    Alloy.Globals.pageStack.open(Alloy.createController('form').getView());
+    return;
+  }
   // get the clicked item from that section
   var item = e.section.getItemAt(e.itemIndex);
 
@@ -48,7 +51,7 @@ function productListClicked(e) {
   var dialog = Ti.UI.createAlertDialog({
     title: 'Yah, Paied?',
     // Get the next payment time, double period from lastPaymentDate, since we still didn't update it
-    message: 'Next payment: ' + moment(item.properties.lastPaymentDate).add(parseInt(item.properties.periodNumber) * 2, item.properties.periodType).fromNow(),
+    message: 'Next payment: ' + Alloy.Globals.moment(item.properties.lastPaymentDate).add(parseInt(item.properties.periodNumber) * 2, item.properties.periodType).fromNow(),
     cancel: 0,
     buttonNames: ['Not yet', 'Yes']
   });
@@ -65,7 +68,7 @@ function productListClicked(e) {
 
     // Set last payment date
     myModel.set({
-      lastPaymentDate: moment(item.properties.lastPaymentDate).add(item.properties.periodNumber, item.properties.periodType).toISOString()
+      lastPaymentDate: Alloy.Globals.moment(item.properties.lastPaymentDate).add(item.properties.periodNumber, item.properties.periodType).toISOString()
     });
 
     myModel.save();
